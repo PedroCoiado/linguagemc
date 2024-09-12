c4b626e526367fde773f047ea9dd788c2bf3de60
Namier <phrc.mc@gmail.com>
2024-09-12 15:00:05 +0000

    Contagem com while

---
+++ contagem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int valor;
+    valor = 10;
+
+    while (valor >= 1)
+    {
+        printf("%d\n",valor);  /* code */
+        valor--; // valor vale 1, incrementar ++;
+
+
+    
+    }
+    return 0;
+
+
+}
